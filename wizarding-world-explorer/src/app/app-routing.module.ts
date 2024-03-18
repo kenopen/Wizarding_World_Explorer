d8f70e039663349ba8209f85ba29bb71251885b7
kenopen <kenneth@opennetworks.com>
2024-03-18 21:21:36 +0200

    add spells routing

--- wizarding-world-explorer/src/app/app-routing.module.ts
+++ wizarding-world-explorer/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HousesComponent } from './components/houses/houses.component';
+import { SpellsComponent } from './components/spells/spells.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/houses', pathMatch: 'full' },
   { path: 'houses', component: HousesComponent },
+  { path: 'spells', component: SpellsComponent },
 ];
 
 @NgModule({

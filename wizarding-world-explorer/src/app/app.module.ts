2f0ad92db61c1e8e59425373cfe98316c883880a
kenopen <kenneth@opennetworks.com>
2024-03-18 22:04:39 +0200

    update toolbar and routing

--- wizarding-world-explorer/src/app/app.module.ts
+++ wizarding-world-explorer/src/app/app.module.ts
@@ -9,9 +9,26 @@ import { ElixirsComponent } from './components/elixirs/elixirs.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Angular Material COmponents
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
-  declarations: [AppComponent, HousesComponent, SpellsComponent, ElixirsComponent, ToolbarComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    HousesComponent,
+    SpellsComponent,
+    ElixirsComponent,
+    ToolbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
